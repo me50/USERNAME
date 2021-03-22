fa55b486833becd7115ce197b1ca5e3f820f48b9
SESHASHAYANAN <76153151+SESHASHAYANAN@users.noreply.github.com>
2021-03-22 21:56:07 +0530

    Add files via upload

---
+++ msss.py
@@ -0,0 +1,61 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:\Users\HOME\Downloads\degrees\degrees.py =============
+Loading data...
+Data loaded.
+Name: Kevin Bacon
+Which 'Kevin Bacon'?
+ID: 102, Name: Kevin Bacon, Birth: 1958
+ID: 9323132, Name: Kevin Bacon, Birth: 
+Intended Person ID: 102
+Name: Tom Cruise
+Traceback (most recent call last):
+  File "C:\Users\HOME\Downloads\degrees\degrees.py", line 139, in <module>
+    main()
+  File "C:\Users\HOME\Downloads\degrees\degrees.py", line 72, in main
+    path = shortest_path(source, target)
+  File "C:\Users\HOME\Downloads\degrees\degrees.py", line 96, in shortest_path
+    raise NotImplementedError
+NotImplementedError
+>>> 
+============== RESTART: C:\Users\HOME\Downloads\degrees\degrees.py ==============
+Loading data...
+Data loaded.
+Name: Kevin Bacon
+Which 'Kevin Bacon'?
+ID: 102, Name: Kevin Bacon, Birth: 1958
+ID: 9323132, Name: Kevin Bacon, Birth: 
+Intended Person ID: 112384
+SystemExit: Person not found.
+>>> 
+============== RESTART: C:\Users\HOME\Downloads\degrees\degrees.py ==============
+Loading data...
+Data loaded.
+Name: Kevin Bacon
+Which 'Kevin Bacon'?
+ID: 9323132, Name: Kevin Bacon, Birth: 
+ID: 102, Name: Kevin Bacon, Birth: 1958
+Intended Person ID: 104257
+SystemExit: Person not found.
+>>> 
+============== RESTART: C:\Users\HOME\Downloads\degrees\degrees.py ==============
+Loading data...
+Data loaded.
+Name: Kevin Bacon
+Which 'Kevin Bacon'?
+ID: 102, Name: Kevin Bacon, Birth: 1958
+ID: 9323132, Name: Kevin Bacon, Birth: 
+Intended Person ID: 9323132
+Name: Tom Cruise
+Not connected.
+>>> 
+============== RESTART: C:\Users\HOME\Downloads\degrees\degrees.py ==============
+Loading data...
+Data loaded.
+Name: Tom Cruise
+Name: Tom Hanks
+2 degrees of separation.
+1: Tom Cruise and Meryl Streep starred in Lions for Lambs
+2: Meryl Streep and Tom Hanks starred in Everything Is Copy
+>>> 
